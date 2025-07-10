5528868d9a022b52930b71c88883274ea09b24e8
unknown <chagai33@gmail.com>
2025-07-10 14:07:22 +0300

    ff

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,20 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 import { getAuth } from 'firebase/auth';
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCQyOiCgzol1rq6kIdHI8tYyFygIA_15bo",
-  authDomain: "shishi-shitufi.firebaseapp.com",
-  databaseURL: "https://shishi-shitufi-default-rtdb.firebaseio.com",
-  projectId: "shishi-shitufi",
-  storageBucket: "shishi-shitufi.firebasestorage.app",
-  messagingSenderId: "342498860740",
-  appId: "1:342498860740:web:9d16f1cc6f732e9003b391",
-  measurementId: "G-LGCND1FDTR"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
 export const database = getDatabase(app);
 export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
