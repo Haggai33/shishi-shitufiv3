01e171aacd56e339fafe52926e554d5657720bdc
unknown <chagai33@gmail.com>
2025-07-16 10:17:07 +0300

    LogDrop vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  // ההגדרה פועלת רק על תהליך הבנייה (build)
+  esbuild: {
+    drop: ['console', 'debugger'],
+  },
   build: {
-    esbuild: {
-      drop: ['console', 'debugger'],
-    },
+    minify: 'esbuild',
   },
 });
